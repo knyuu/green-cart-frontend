bb7c32a9a6139bcd355e0400813597c852e38dbd
KhanhDu1622 <khanhdu1622@gmail.com>
2025-04-25 11:23:24 +0700

    Commit 04
    Hoàn thiện UI của các trang:
    - My Orders
    - Cart
    - Login của Admin
    -Một phần UI của Admin Dashboard

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -7,7 +7,7 @@ export const AppContext = createContext();
 
 export const AppContextProvider = ({children}) => {
 
-    const currency = import.meta.VITE_CURRENCY;
+    const currency = import.meta.env.VITE_CURRENCY;
 
     const navigate = useNavigate();
     const [user, setUser] = useState(null);
@@ -56,12 +56,33 @@ export const AppContextProvider = ({children}) => {
         toast.success("Removed From Cart");
     }
 
+    // Get Cart Item Count
+    const getCartCount = () => {
+        let totalCount = 0;
+        for(const item in cartItems){
+            totalCount += cartItems[item];
+        }
+        return totalCount;
+    }
+
+    //Get Cart Total Amount
+    const getCartAmount = () => {
+        let totalAmount = 0;
+        for (const items in cartItems){
+            let itemInfo = products.find((product) => product._id === items);
+            if(cartItems[items] > 0){
+                totalAmount += itemInfo.offerPrice * cartItems[items];
+            }
+        }
+        return Math.floor(totalAmount * 100) / 100;
+    }
+
     useEffect(() => {
         fectchProducts();
     }, []);
 
     const value = {navigate, user, setUser, isSeller, setIsSeller, showUserLogin, setShowUserLogin, products, currency, cartItems, addToCart, updateCartItem, removeFromCart
-        ,searchQuery, setSearchQuery
+        ,searchQuery, setSearchQuery, getCartAmount, getCartCount
     }
     
     return <AppContext.Provider value={value}>
